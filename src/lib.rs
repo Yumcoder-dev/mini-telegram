271a5e8f3baa0fcd3db4b1e16afb0bd28e728573
yumcoder <yumcoder.platform@gmail.com>
2023-02-06 20:50:19 +0330

    add minimal doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 // Copyright 2022 mini-telegram Project Authors. Licensed under Apache-2.0.
 
+//! A minimal (i.e. very incomplete) implementation of a [MTProto](https://core.telegram.org/mtproto) (telegram) server.
+//!
+//! The purpose of this project is to provide a larger example of an
+//! asynchronous Rust project built with Tokio. Do not attempt to run this in
+//! production... seriously.
+//!
+//! The major components are:
+//!
+//! * `server`: MTProto(telegram) server implementation.
+//!
+//! * `client`: an asynchronous MTProto client implementation.
+//!
+//! * `frame`: represents a single MTProto protocol frame. A frame is used as an
+//!   intermediate representation between a "command" and the byte
+//!   representation.
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
